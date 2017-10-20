104e00483e29d1735ddf503ef8e55bd602b726af
Juan Ramírez <Juan Ramrez@10.230.168.243>
2017-10-20 11:57:08 +0100

    Versión 4

--- src/kata3/Kata3.java
+++ src/kata3/Kata3.java
@@ -4,24 +4,11 @@ public class Kata3 {
 
     public static void main(String[] args) {
         Histogram histo = new Histogram();
-        histo.increment("gmail.com");
-        histo.increment("gmail.com");
-        histo.increment("gmail.com");
-        histo.increment("gmail.com");
-        histo.increment("gmail.com");
-        histo.increment("hotmail.com");
-        histo.increment("hotmail.com");
-        histo.increment("hotmail.com");
-        histo.increment("hotmail.com");
-        histo.increment("hotmail.com");
-        histo.increment("hotmail.com");
-        histo.increment("hotmail.com");
-        histo.increment("ulpgc.es");
-        histo.increment("ulpgc.es");
-        histo.increment("ulpgc.es");
-        histo.increment("ulpgc.es");
-        histo.increment("ulpgc.es");
-        histo.increment("ull.com");
+        
+        histo.increment("gmail.com", 5);
+        histo.increment("hotmail.com", 10);
+        histo.increment("ulpgc.es", 7);
+        histo.increment("ull.es", 8);
         
         new HistogramDisplay(histo).execute();
     }
